e4564c66e161f47be4214e06134831ca3241aecc
Ryoma Jodoi <sup0ry0jodoi@gmail.com>
2021-03-13 23:34:53 +0900

    Set up Amplify

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,62 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div v-if="authState !== 'signedin'">You are signed out.</div>
+    <amplify-authenticator>
+      <div v-if="authState === 'signedin' && user">
+        <div>Hello, {{user.username}}</div>
+      </div>
+      <h1>Todo App</h1>
+      <input type="text" v-model="name" placeholder="Todo name">
+      <input type="text" v-model="description" placeholder="Todo description">
+      <button v-on:click="createTodo">Create Todo</button>
+      <div v-for="item in todos" :key="item.id">
+        <h3>{{ item.name }}</h3>
+        <p>{{ item.description }}</p>
+      </div>
+      <amplify-sign-out></amplify-sign-out>
+    </amplify-authenticator>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { API } from 'aws-amplify';
+import { createTodo } from './graphql/mutations';
+import { onAuthUIStateChange } from '@aws-amplify/ui-components'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'AuthStateApp',
+  created() {
+    this.unsubscribeAuth = onAuthUIStateChange((authState, authData) => {
+      this.authState = authState;
+      this.user = authData;
+    })
+  },
+  data() {
+    return {
+      user: undefined,
+      authState: undefined,
+      unsubscribeAuth: undefined,
+      name: '',
+      description: '',
+      todos: []
+    }
+  },
+  beforeUnmount() {
+    this.unsubscribeAuth();
+  },
+  methods: {
+    async createTodo() {
+      const { name, description } = this;
+      if (!name || !description) return;
+      const todo = { name, description };
+      this.todos = [...this.todos, todo];
+      await API.graphql({
+        query: createTodo,
+        variables: {input: todo},
+      });
+      this.name = '';
+      this.description = '';
+    },
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
